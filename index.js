1be566f1f76b00103d37ec29b6165a10bc87bf30
shirodkarpushkar <pushkars@winjit.com>
2020-04-30 10:33:28 +0530

    created get request

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 
 var todoList = [
-    {
-        id: 1,
-        description: "Bring milk as told by mom",
-        completed: false
-    },
-    {
-        id: 2,
-        description: "Comb your hair",
-        completed: false
-    }
-]
-    
-    
+  {
+    id: 1,
+    description: "Bring milk as told by mom",
+    completed: false,
+  },
+  {
+    id: 2,
+    description: "Comb your hair",
+    completed: false,
+  },
+];
+
 app.get("/", (req, res) => {
   res.send("Welcome to TODO LIST");
 });
-app.get("/todo", (req, res) => {
-  res.send("Welcome to TODO LIST");
+// get all todos 
+app.get("/todos", (req, res) => {
+  res.json(todoList);
 });
 
 app.listen(PORT, () => {
