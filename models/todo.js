4198b29ddccf83d0939a2e2847efd958208cf449
shirodkarpushkar <pushkars@winjit.com>
2020-05-04 11:08:24 +0530

    minor bug fixes

--- models/todo.js
+++ models/todo.js
@@ -3,7 +3,6 @@ module.exports = function (sequelize, DataTypes) {
       description: {
         type: DataTypes.STRING,
         allowNull: false,
-
         validate: {
           len: [1, 250],
         },
