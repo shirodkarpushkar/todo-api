3daebf557daaaf218efa0ef6ea1f92e01ee158e6
shirodkarpushkar <pushkars@winjit.com>
2020-05-04 12:23:09 +0530

    ADDED pg pg-hstore package

--- db.js
+++ db.js
@@ -1,9 +1,19 @@
 var Sequelize = require("sequelize");
 const Op = Sequelize.Op;
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-  dialect: "sqlite",
-  storage: __dirname + "/data/dev-todo-api.sqlite",
-});
+var sequelize;
+
+var env = process.env.NODE_ENV || "development";
+
+if (env === "production") {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+  });
+} else {
+  sequelize = new Sequelize(undefined, undefined, undefined, {
+    dialect: "sqlite",
+    storage: __dirname + "/data/dev-todo-api.sqlite",
+  });
+}
 
 var db = {};
 db.todo = sequelize.import(__dirname + "/models/todo.js");
