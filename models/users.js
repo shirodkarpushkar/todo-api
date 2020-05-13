6041d095ee36e06dcb5a26b44dd56a432bca4c76
shirodkarpushkar <pushkars@winjit.com>
2020-05-13 18:04:09 +0530

    added becrypt salted hashed password

--- models/users.js
+++ models/users.js
@@ -1,3 +1,5 @@
+var bcrypt = require("bcrypt");
+var _ = require("underscore");
 module.exports = function (sequelize, DataTypes) {
   return sequelize.define(
     "users",
@@ -11,23 +13,36 @@ module.exports = function (sequelize, DataTypes) {
         },
       },
       password: {
-        type: DataTypes.STRING,
+        type: DataTypes.VIRTUAL,
         allowNull: false,
         defaultValue: false,
         validate: {
           len: [7, 100],
         },
+          set: (value) => {
+              var salt = bcrypt.genSaltSync(10)
+              var hashedPassword = bcrypt.hashSync(value, salt)
+              this.setDataValue('password',value)
+              this.setDataValue("salt", salt);
+              this.setDataValue("password_hash", hashedPassword);
+              
+        },
+      },
+      salt: {
+        type: DataTypes.STRING,
+      },
+      password_hash: {
+        type: DataTypes.STRING,
       },
     },
-      {
-          hooks: {
-              beforeValidate: (user, options) => {
-                  if (typeof user.email === "string") {
-                      user.email  = user.email.toLowerCase()
-                  }
-                  
-            }
-        }
+    {
+      hooks: {
+        beforeValidate: (user, options) => {
+          if (typeof user.email === "string") {
+            user.email = user.email.toLowerCase();
+          }
+        },
+      },
     }
   );
 };
